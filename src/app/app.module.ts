c69a2da38d46b83d495ed44ebb9e759e128a48c2
ilhan ALTIN <ilhanaltin79@gmail.com>
2019-11-26 13:08:57 +0300

    Initial Component run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,6 @@ import {APP_BASE_HREF} from '@angular/common';
     CKEditorModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
-  bootstrap: [AppComponent,TestComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
