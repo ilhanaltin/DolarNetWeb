30dd3a0224526bc8ab4558a69fc9aaa499756fa2
ilhan ALTIN <ilhanaltin79@gmail.com>
2020-02-06 13:40:08 +0300

    refactor

--- src/@dolarnet/dolarnet-config/api.config.ts
+++ src/@dolarnet/dolarnet-config/api.config.ts
@@ -4,8 +4,8 @@ export const apiConfig = {
     {
       Main:
       {
-        //Url: "https://api-dolar.com/api/",
-        Url: "https://localhost:44366/api/"
+        Url: "https://api-dolar.com/api/",
+        //Url: "https://localhost:44366/api/"
       },
       CollectApi:
       {
